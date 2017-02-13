50cf486d2023bc1a1e30e4f2e8a2e04bea9e0af2
u1674145 <u1674145@unimail.hud.ac.uk>
2017-02-13 13:09:11 +0000

    Week two or five

--- app/src/main/java/uk/edu/syntaxerror/ewallet/Login.java
+++ app/src/main/java/uk/edu/syntaxerror/ewallet/Login.java
@@ -1,10 +1,14 @@
 package uk.edu.syntaxerror.ewallet;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class Login extends AppCompatActivity {
+
+    //some random useleess stuff. for now.
+    // public final static String LOGIN_DATA = "uk.edu.syntaxerror.ewallet.LOGIN_DATA";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +20,7 @@ public class Login extends AppCompatActivity {
     }
     public  void registerAction(View view) {
         //TODO: register action
-        setContentView(R.layout.activity_list_item);
+        Intent intent = new Intent(this,RegisterActivity.class);
+        startActivity(intent);
     }
 }
