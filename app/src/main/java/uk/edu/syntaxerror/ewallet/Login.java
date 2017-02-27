c09b8b980d42826ac7fe5edf4b4a805734739ed9
K.Szymanski U1674145 <U1674145@unimail.hud.ac.uk>
2017-02-27 13:15:12 +0000

    Week five

--- app/src/main/java/uk/edu/syntaxerror/ewallet/Login.java
+++ app/src/main/java/uk/edu/syntaxerror/ewallet/Login.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
 
@@ -17,7 +19,21 @@ public class Login extends AppCompatActivity {
     }
     public void loginAction(View view) {
         //TODO: login action to take place
+        TextView email_text = (TextView) findViewById(R.id.login_email);
+        TextView password_text = (TextView) findViewById(R.id.login_password);
+
+        LoginAction login = new LoginAction(email_text.getText().toString(), password_text.getText().toString());
+        if(!login.isValidEmail()) {
+            //TODO: ERRROR msg - not valid Email
+        }else if (!login.isValidPassword()) {
+            //TODO: ERROR msg - not valid password
+        }else {
+            Intent intent = new Intent(this, MainHub.class);
+            intent.putExtra("login", true);
+            startActivity(intent);
+        }
     }
+
     public  void registerAction(View view) {
         //TODO: register action
         Intent intent = new Intent(this,RegisterActivity.class);
