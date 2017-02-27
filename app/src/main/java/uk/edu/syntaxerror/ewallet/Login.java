15bcc4648223811cf44897cf781c20b4b746945f
u1674145 <u1674145@unimail.hud.ac.uk>
2017-02-27 13:18:32 +0000

    Week 7

--- app/src/main/java/uk/edu/syntaxerror/ewallet/Login.java
+++ app/src/main/java/uk/edu/syntaxerror/ewallet/Login.java
@@ -1,10 +1,9 @@
 package uk.edu.syntaxerror.ewallet;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
@@ -35,7 +34,6 @@ public class Login extends AppCompatActivity {
     }
 
     public  void registerAction(View view) {
-        //TODO: register action
         Intent intent = new Intent(this,RegisterActivity.class);
         startActivity(intent);
     }
