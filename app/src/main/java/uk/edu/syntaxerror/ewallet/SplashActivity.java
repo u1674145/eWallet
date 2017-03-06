1bbdcab6c4341f165f13e114bd31968dbdf7ce02
u1674145 <u1674145@unimail.hud.ac.uk>
2017-03-06 12:53:54 +0000

    Week 8

---
+++ app/src/main/java/uk/edu/syntaxerror/ewallet/SplashActivity.java
@@ -0,0 +1,19 @@
+package uk.edu.syntaxerror.ewallet;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+        finish();
+
+    }
+}
